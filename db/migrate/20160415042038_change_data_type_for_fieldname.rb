decb18ece676b0b9142056e9aaf9b8a44132bbcd
julio <julio336@hotmail.com>
2016-05-06 19:23:28 -0500

    heroku

--- db/migrate/20160415042038_change_data_type_for_fieldname.rb
+++ db/migrate/20160415042038_change_data_type_for_fieldname.rb
@@ -1,9 +1,9 @@
 class ChangeDataTypeForFieldname < ActiveRecord::Migration
   def self.up
     change_table :rooms do |t|
-      t.change :date_occupied, 'date USING date_occupied::date'
-      t.change :date_start, 'date USING date_occupied::date'
-      t.change :date_end, 'date USING date_occupied::date'
+      t.change :date_occupied, 'date USING date_occupied::date', :datetime
+      t.change :date_start, 'date USING date_occupied::date', :datetime
+      t.change :date_end, 'date USING date_occupied::date', :datetime
     end
   end
   def self.down
