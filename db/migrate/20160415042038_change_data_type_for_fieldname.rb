09a17d213df8ad74ed17ed6cd07461e5e379ccba
julio <julio336@hotmail.com>
2016-05-06 19:36:38 -0500

    db

--- db/migrate/20160415042038_change_data_type_for_fieldname.rb
+++ db/migrate/20160415042038_change_data_type_for_fieldname.rb
@@ -1,9 +1,9 @@
 class ChangeDataTypeForFieldname < ActiveRecord::Migration
   def self.up
     change_table :rooms do |t|
-      t.change :date_occupied, 'ALTER TABLE :date_occupied ALTER COLUMN :date_time type :date USING to_date(date_time, '2016:01:01')'
-      t.change :date_start, 'ALTER TABLE :date_occupied ALTER COLUMN :date_time type :date USING to_date(date_time, '2016:01:01')'
-      t.change :date_end, 'ALTER TABLE :date_occupied ALTER COLUMN :date_time type :date USING to_date(date_time, '2016:01:01')'
+      t.change :date_occupied, 'ALTER TABLE :date_occupied ALTER COLUMN :date_time type :date USING to_date(date_time, 'YYYY:MM:DD')'
+      t.change :date_start, 'ALTER TABLE :date_occupied ALTER COLUMN :date_time type :date USING to_date(date_time, 'YYYY:MM:DD')'
+      t.change :date_end, 'ALTER TABLE :date_occupied ALTER COLUMN :date_time type :date USING to_date(date_time, 'YYYY:MM:DD')'
     end
   end
   def self.down
