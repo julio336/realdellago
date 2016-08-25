71e0a6575c547f71144a5afe29600b3b07fe9d92
julio <julio336@hotmail.com>
2016-08-24 21:25:32 -0500

    update page

--- app/controllers/charges_steps_controller.rb
+++ app/controllers/charges_steps_controller.rb
@@ -4,7 +4,15 @@ class ChargesStepsController < ApplicationController
 	steps :calendar, :room_select, :payment
 
 	def show
+		@contact = Contact.new(params[:contact])
+	    @contact.request = request
+	    if @contact.deliver
+	      flash.now[:notice] = 'Gracias por tu mensaje, pronto nos pondremos en contacto contigo.'
+	    else
+	      flash.now[:error] = 'El mensaje no se pudo envíar, por favorintentelo más tarde.'
+	    end
 		render_wizard
+
 	end
 
 end
